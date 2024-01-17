8ea6f1dc280d8b6a3de77f2f5014318675442b85
adavydenko <adavydenko@elinext.com>
2024-01-17 15:37:48 +0300

    feat: add http server

--- httpServer.js
+++ httpServer.js
@@ -2,35 +2,53 @@ import http from 'http';
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/friends') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(
-      {
-        id: 1,
-        name: 'Sir Isaac Newton'
-      }
-    ))
-  } else if (req.url === '/messages') {
-    res.setHeader('Content-Type', 'text/html')
+const friends = [
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  },
+  {
+    id: 2,
+    name: 'Nicola Tesla',
+  },
+];
 
-    res.write('<html>')
-    res.write('<body>')
-    res.write('<ul>')
-    res.write('<li>Hello Isaac Newton!</li>')
-    res.write('<li>What are your thoughts on astronomy?</li>')
-    res.write('</ul>')
-    res.write('</body>')
-    res.write('</html>')
-    res.end()
+const server = http.createServer((req, res) => {
+  const items = req.url.split('/');   // /friends/2 => ['', 'friends', ''2]
+  if (req.method === 'POST' && items[1] === 'friends') {
+    req.on('data', (data) => {
+      const friend = data.toString();
+      console.log('Request:', friend);
+      friends.push(JSON.parse(friend));
+    });
+    req.pipe(res); //readable.pipe(writable)
+  } else if (req.method === 'GET' && items[1] === 'friends') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    if (items.length === 3) {
+      res.end(JSON.stringify(friends[items[2]]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === 'messages') {
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello Isaac Newton!</li>');
+    res.write('<li>What are your thoughts on astronomy?</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
   } else {
-    res.statusCode = 404
-    res.end()
+    res.statusCode = 404;
+    res.end();
   }
 
-})
+});
 
 server.listen(PORT, () => {
-  console.log('Listening on port 3000')
-})
+  console.log('Listening on port 3000');
+});
