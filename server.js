b7ea33f3fa1d6d157c2d8e9ae9f27e5307dce954
adavydenko <adavydenko@elinext.com>
2024-01-23 17:10:53 +0300

    feat: add routers for different collection of data

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { getMessages, postMessage } from './controllers/messages.controller.js';
-import { getFriend, getFriends, postFriend } from './controllers/friends.controller.js';
+import { friendsRouter } from './routes/friends.router.js';
+import { messagesRouter } from './routes/messages.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -9,17 +9,13 @@ app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.get('/friends', getFriends);
-app.get('/friends/:friendId', getFriend);
-app.post('/friends', postFriend);
-
-app.get('/messages', getMessages);
-app.post('/messages', postMessage);
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
