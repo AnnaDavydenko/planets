4a06d84f1dfe430f02d76c91fd109d72dedc0a29
adavydenko <adavydenko@elinext.com>
2024-02-14 17:44:17 +0300

    feat: add Logging Requests With Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { planetsRouter } from './routes/planets/planets.router.js';
-import cors from 'cors'
+import cors from 'cors';
+import morgan from 'morgan';
 
 export const app = express();
 
 app.use(cors({
   origin: 'http://localhost:5173'
 }))
+
+app.use(morgan('combined'))
+
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
