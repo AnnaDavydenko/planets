0d26436b0d708dacaee57ebd44c4c81e16d9e3ff
Anna Davydenko <adavydenko@elinext.com>
2024-05-02 17:35:14 +0300

    feat: add deploy-cluster pm2 command

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,23 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { planetsRouter } from './routes/planets/planets.router.js';
 import cors from 'cors';
 import morgan from 'morgan';
 import { launchesRouter } from './routes/launches/launches.router.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173'
-}))
+  origin: 'http://localhost:5173',
+}));
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, '..', 'public')));
-// app.get('/*', (res, req) => {
-//   res.sendFile(path.join(__dirname, '..', 'public', 'index.html')
-// })
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/planets', planetsRouter);
 app.use('/launches', launchesRouter);
