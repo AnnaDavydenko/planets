0b4924df46d120d6965fdd227156a6c9f90b9209
Anna Davydenko <adavydenko@elinext.com>
2024-05-13 15:59:31 +0300

    feat: connect with the latest version of mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,28 @@
 import http from 'http';
 import { app } from './app.js';
 import { loadPlanetsData } from './models/planets.model.js';
+import mongoose from 'mongoose';
 
 const PORT = process.env.PORT || 8000;
-
+const MONGO_URL = 'mongodb+srv://hannaworkaccout:34274TXruB5jaEnD@nasacluster.3kjv2vd.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster';
 const server = http.createServer(app);
 
-await loadPlanetsData();
-server.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}...`)
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
 });
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
+async function startServer() {
+  await mongoose.connect(MONGO_URL);
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}...`);
+  });
+}
+
+startServer();
+
 
