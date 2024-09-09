9020cfe402d6b3c0af723d529c7269ae498b639a
Anna Davydenko <adavydenko@elinext.com>
2024-09-09 16:50:26 +0300

    feat: use helmet

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import * as https from 'https';
 import { readFileSync } from 'node:fs';
+import helmet from 'helmet';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
 const app = express();
+
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
   return res.send('Your secret value is 42!');
